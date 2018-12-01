1c50ae4c041b8b48894e17cb8173b0503c5c09da
DingSun <dings@scantis.com>
2018-12-01 22:14:16 +0800

    save

--- haiyi/views.py
+++ haiyi/views.py
@@ -2,6 +2,9 @@ from django.http import HttpResponse
 import datetime
 from django.views.decorators.csrf import csrf_exempt
 import json
+import logging
+
+logger = logging.getLogger(__name__)
 
 @csrf_exempt
 def current_datetime(request):
@@ -11,5 +14,6 @@ def current_datetime(request):
 
 @csrf_exempt
 def chat_receiver(request):
+    logger.info('from wechat:', request.POST)
     html = "<html><body>chat: %s.</body></html>" % request.POST
     return HttpResponse(html)
