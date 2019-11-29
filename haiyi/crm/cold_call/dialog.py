913f1b1ad43bc70a1deea49945a5a0054ef1fb2c
DingSun <dings@scantis.com>
2019-11-30 00:17:12 +0800

    parse dialog

--- haiyi/crm/cold_call/dialog.py
+++ haiyi/crm/cold_call/dialog.py
@@ -5,6 +5,7 @@ from haiyi.tools.es_handler import dialog_search
 from haiyi.tools.es_handler import bulk_index_1
 import logging
 import re
+import openpyxl
 
 logger = logging.getLogger(__name__)
 
@@ -12,8 +13,7 @@ customers = {}
 
 
 def init():
-    filename = "dialog_script.yaml"
-    bulk_index_1(index=get_index(), generator=dialog_index)
+    bulk_index_1(index=get_index(), generator=dialog_index_excel)
 
 
 def get_index():
@@ -23,6 +23,65 @@ def get_index():
 def uuid_question(question):
     uuid_x = uuid.uuid3(uuid.NAMESPACE_DNS, question)
     return str(uuid_x)
+
+
+def dialog_index_excel():
+    def __cell(sheet, column_label, row_id):
+        v = sheet[f"{column_label}{row_id}"].value
+        if not v:
+            return v
+        v = str(v).replace("\"", "").replace("\n", "")
+        try:
+            d = int(v)
+            return d
+        except Exception as e:
+            return v
+
+    class unit(object):
+        question = ""
+        answers = []
+
+    all = "A,B,C,D,E,F"
+
+    wb = openpyxl.load_workbook("话术表格整理.xlsx", data_only=True)
+    sheet = wb["Sheet1"]
+    row_id = 4
+    u = None
+
+    while True:
+        if not __cell(sheet, "C", row_id):
+            break
+        if __cell(sheet, "A", row_id):
+            if u:
+                data = {
+                    "_id": uuid_question(u.question),
+                    "_index": "dasdas",
+                    "_type": "doc",  # '_type' field is discouraged since ES 6.x, just use the 'doc' as default
+                    "_source": {
+                        "question": u.question,
+                        "answers": u.answers
+                    },
+                    "doc_as_upsert": True,
+                    "_op_type": 'index'
+                }
+                print(row_id)
+                yield data
+
+            u = unit()
+            u.question = __cell(sheet, "B", row_id)
+            customers = all
+            if __cell(sheet, "D", row_id):
+                customers = __cell(sheet, "D", row_id)
+            anw = "%s|%s" % (customers, __cell(sheet, "C", row_id))
+            u.answers.append(anw)
+        if __cell(sheet, "C", row_id):
+            if u:
+                customers = all
+                if __cell(sheet, "D", row_id):
+                    customers = __cell(sheet, "D", row_id)
+                anw = "%s|%s" % (customers, __cell(sheet, "C", row_id))
+                u.answers.append(anw)
+        row_id += 1
 
 
 def dialog_index():
